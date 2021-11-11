1274449a1d4bdf4e46197c91e45ed863db2f2684
Max <for_maxim@bk.ru>
2021-11-11 16:00:35 +0300

    add printf_value

--- lab2/uint1024.c
+++ lab2/uint1024.c
@@ -22,6 +22,38 @@ uint1024_t init(){
     return num;
 }
 
+void printf_value(uint1024_t x){
+    char10 result = init10();
+    char10 degree = init10();
+    degree.n[0] = 1;
+    for(int i = 0; i <= x.index; ++i){
+        int cur = (x.n[i / 8] >> (7 - i % 8)) & 1;
+        if (cur){
+            result = add10(result, degree);
+        }
+        degree = add10(degree, degree);
+    }
+    printf10(result);
+    free_char10(result);
+    free_char10(degree);
+}
+
+void fprintf_value(uint1024_t x, FILE *file){
+    char10 result = init10();
+    char10 degree = init10();
+    degree.n[0] = 1;
+    for(int i = 0; i <= x.index; ++i){
+        int cur = (x.n[i / 8] >> (7 - i % 8)) & 1;
+        if (cur){
+            result = add10(result, degree);
+        }
+        degree = add10(degree, degree);
+    }
+    fprintf10(result, file);
+    free_char10(result);
+    free_char10(degree);
+}
+
 int main(void) {
     uint1024_t a = init();
     uint1024_t b = init();
