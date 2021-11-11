2f627ed48a4c87489e93e6a673550e0cbfa38fdc
Max <for_maxim@bk.ru>
2021-11-11 15:54:39 +0300

    add mult_op

--- lab2/uint1024.c
+++ lab2/uint1024.c
@@ -22,6 +22,31 @@ uint1024_t init(){
     return num;
 }
 
+uint1024_t mult_op(uint1024_t x, uint1024_t y){
+    uint1024_t result = init();
+    
+    for(int i = x.index; i >= 0; --i){
+    
+        int cur = (x.n[i / 8] >> (7 - i % 8)) & 1;
+
+        if(cur){
+            uint1024_t tmp = init();
+            for(int j = 0; j <= y.index; ++j){
+                int cury = (y.n[j / 8] >> (7 - j % 8)) & 1;
+                int cur_index = i + j;
+                tmp.n[cur_index / 8] = (char) ((tmp.n[cur_index / 8] >> (7 - cur_index % 8 )) | (cury)) << (7 - cur_index % 8);
+            }
+
+            result = add_op(result, tmp);
+    }
+
+  }
+
+    result.index = find_index(result);
+
+    return result;
+}
+
 int main(void) {
     uint1024_t a = init();
     uint1024_t b = init();
