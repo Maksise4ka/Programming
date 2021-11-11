f7646cd877091c896972381234e6b5fa40044352
Max <for_maxim@bk.ru>
2021-11-11 15:53:33 +0300

    add function subtr_op

--- lab2/uint1024.c
+++ lab2/uint1024.c
@@ -22,6 +22,41 @@ uint1024_t init(){
     return num;
 }
 
+uint1024_t subtr_op(uint1024_t x, uint1024_t y){
+    uint1024_t result = init();
+    int prev = 0;
+
+    for(int i = 0; i < 128; ++i){
+
+        for(int j = 0; j < 8; ++j){
+      
+            int digx = (x.n[i] >> (7 - j)) & 1;
+            int digy = (y.n[i] >> (7 - j)) & 1;
+
+            if ((digx && digy && !prev) || (digx && !digy && prev) || (!digx && !digy && !prev) || (!digx && digy && prev))
+                result.n[i] = (char) ((result.n[i] >> (7 - j)) | (0)) << (7 - j);
+            else
+                result.n[i] = (char) ((result.n[i] >> (7 - j)) | (1)) << (7 - j);
+
+            prev = 1 ?(digx < digy + prev) : 0;
+
+            int cur = (result.n[i] >> (7 - j)) & 1;
+            if (cur)
+                result.index = 8 * i + j;
+
+        }
+
+    }
+
+    if(prev == 1){
+        printf("UB\n");
+        return init();
+    }
+
+    return result;
+
+}
+
 int main(void) {
     uint1024_t a = init();
     uint1024_t b = init();
