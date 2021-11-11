adf17cdaced7c94f2f8ad8145a1326ce86cbfe7d
Max <for_maxim@bk.ru>
2021-11-11 15:46:48 +0300

    add function from_uint

--- lab2/uint1024.c
+++ lab2/uint1024.c
@@ -22,6 +22,32 @@ uint1024_t init(){
     return num;
 }
 
+// определяет размер числа
+int find_index(uint1024_t x){
+
+  for(int i = 1024; i >= 0; -- i){
+    int cur = (x.n[i / 8] >> (7 - i % 8)) & 1;
+      if (cur)
+        return i;
+  }
+  return 0;
+
+}
+
+
+uint1024_t from_uint(unsigned int x){
+    uint1024_t num = init();
+    while (x != 0){
+        int cur = num.index;
+        num.n[cur / 8] = (char) ((num.n[cur / 8] >> (7 - cur % 8)) | (x % 2)) << (7 - cur % 8);
+        num.index ++;
+        x /= 2;
+    }
+    num.index --;
+
+    return num;
+}
+
 int main(void) {
     uint1024_t a = init();
     uint1024_t b = init();
