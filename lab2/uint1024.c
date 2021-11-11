fd87dd20de90f4374d5ccfc39aebfb1ac78cb26b
Max <for_maxim@bk.ru>
2021-11-11 16:02:39 +0300

    add scanf_value and destructor

--- lab2/uint1024.c
+++ lab2/uint1024.c
@@ -22,6 +22,53 @@ uint1024_t init(){
     return num;
 }
 
+void free_uint(uint1024_t x){
+    free(x.n);
+}
+
+void scanf_value(uint1024_t* x){
+    char *s = (char *) malloc(310 * sizeof(char));
+    scanf("%s", s);
+    uint1024_t result = init();
+    uint1024_t digit = from_uint(10);
+    for (int i = 0; i <= (int) strlen(s) - 1; ++i){
+        result = mult_op(result, digit);
+        uint1024_t cur_digit = from_uint((unsigned int) (s[i] - '0'));
+        result = add_op(result, cur_digit);
+    }
+
+    *x = result;
+    free(s);
+}
+
+void fscanf_value(uint1024_t* x, FILE *file){
+    char *s = (char *) malloc(310 * sizeof(char));
+    fscanf(file, "%s", s);
+    uint1024_t result = init();
+    uint1024_t digit = from_uint(10);
+    for (int i = 0; i <= (int) strlen(s) - 1; ++i){
+        result = mult_op(result, digit);
+        uint1024_t cur_digit = from_uint((unsigned  int) (s[i] - '0'));
+        result = add_op(result, cur_digit);
+    }
+
+    *x = result;
+    free(s);
+}
+
+
+
+
+//выводит число в двоичном виде в обратном порядке разрядов
+void bin_exp(uint1024_t x){
+    x.index = find_index(x);
+    for(int i = 0; i <= x.index; ++i){
+        printf("%d", (x.n[i / 8] >> (7 - i % 8 )) & 1);
+    }
+
+    printf("\n");
+}
+
 int main(void) {
     uint1024_t a = init();
     uint1024_t b = init();
